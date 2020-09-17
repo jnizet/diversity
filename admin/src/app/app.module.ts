e81ed49a7be0e1b83c84b66dbd3618127cd2c371
cexbrayat <cedric@ninja-squad.com>
2020-09-17 12:02:15 +0200

    chore: add prettier and format

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -7,15 +7,9 @@ import { HomeComponent } from './home/home.component';
 import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- admin/src/app/app.routes.ts
+++ admin/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-export const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent }
-];
+export const APP_ROUTES: Routes = [{ path: '', component: HomeComponent }];
