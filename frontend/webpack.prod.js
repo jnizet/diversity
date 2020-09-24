44352e76c22b4b11db8990458aab9b33f4a2169c
Jean-Baptiste Nizet <jb@ninja-squad.com>
2020-09-24 14:56:53 +0200

    feat: precompress css and js bundles, in gzip and brotli
    This has several advantages:
    
    - the server doesn't need to compress them: it just serves the pre-compressed ones
    - it work-arounds the issue https://github.com/spring-projects/spring-boot/issues/23464: which causes the JS bundle not to be compressed at all
    - it provides brotli compression, which is slightly more efficient

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,6 +1,23 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  plugins: [new CompressionPlugin()],
+};
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new CompressionPlugin({
+      filename: '[path][base].gz'
+    }),
+    new CompressionPlugin({
+      filename: '[path][base].br',
+      algorithm: 'brotliCompress',
+      compressionOptions: {
+        level: 11
+      }
+    })
+  ]
 });
