907611b03f6afded96cdd3253377718b5bf14e0e
jnizet <jb@ninja-squad.com>
2020-11-04 09:17:12 +0100

    feat: integrate version 2.4
    except for maps and territory

--- frontend/src/science/science-controller.ts
+++ frontend/src/science/science-controller.ts
@@ -1,15 +1,17 @@
 import { Controller } from 'stimulus';
 import Swiper, { Navigation, Pagination, EffectFade, Autoplay } from 'swiper';
+import $ from 'jquery';
 
 export class ScienceController extends Controller {
   connect() {
     // configure Swiper to use modules
     Swiper.use([Navigation, Pagination, EffectFade, Autoplay]);
 
-    new Swiper('.swiper-science', {
+    const topSwiper = new Swiper('.swiper-science', {
       speed: 1000,
       keyboard: true,
       effect: 'fade',
+      freeMode: true,
       loop: true,
       fadeEffect: {
         crossFade: true
@@ -23,6 +25,13 @@ export class ScienceController extends Controller {
         clickable: true,
         bulletClass: 'bullet-white',
         bulletActiveClass: 'active-white'
+      }
+    });
+
+    topSwiper.on('slideChange', function () {
+      if (window.matchMedia('(max-width: 990px)').matches) {
+        const body = $('html, body');
+        body.stop().animate({ scrollTop: $('.section-science-exemple').offset().top - 60 }, 500, 'swing');
       }
     });
   }
