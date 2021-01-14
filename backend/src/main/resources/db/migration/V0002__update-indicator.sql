ddf29c5a2c7d99aaae89d859eb7b526ea77af454
arnaud.monteils@mnhn.fr <arnaud.monteils@mnhn.fr>
2021-01-14 11:30:24 +0100

    fix: fix is rounded column creation on indicator table

--- backend/src/main/resources/db/migration/V0002__update-indicator.sql
+++ backend/src/main/resources/db/migration/V0002__update-indicator.sql
@@ -2,6 +2,6 @@ ALTER TABLE indicator
 ADD COLUMN is_rounded boolean;
 
 UPDATE indicator
-SET is_rounded = false where is_rounded = null;
+SET is_rounded = false where is_rounded is null;
 
 ALTER TABLE indicator ALTER COLUMN is_rounded SET NOT NULL;
